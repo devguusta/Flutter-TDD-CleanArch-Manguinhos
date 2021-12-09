916f5a31bb99c717a3d2260b96cf1a015c721110
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2021-12-09 20:27:00 -0300

    corrige test http client correct url

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,6 +1,9 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+
+import 'remote_authentication_test.mocks.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -16,11 +19,10 @@ abstract class HttpClient {
   Future<void> request({required String url}) async {}
 }
 
-class HttpClientSpy extends Mock implements HttpClient {}
-
+@GenerateMocks([HttpClient])
 void main() {
   test("Should call HttpClient With correct URL", () async {
-    final httpClient = HttpClientSpy();
+    final httpClient = MockHttpClient();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthentication(httpClient: httpClient, url: url);
     await sut.auth();

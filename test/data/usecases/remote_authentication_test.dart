4f6d2709c618edb057b33108952b100a645a3433
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2021-12-12 19:55:51 -0300

    ensure RemoteAuthentication throws UnexpectedError on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -35,7 +35,7 @@ void main() {
         method: 'post',
         body: {'email': params.email, 'password': params.secret}));
   });
-  test("Shouldthrow UnexpectedError if HttpClient returns 400", () async {
+  test("Shouldthrow UnexpectedError if HttpClient returns 404", () async {
     when(
       httpClient.request(
         url: "url",
@@ -47,7 +47,22 @@ void main() {
       ),
     ).thenThrow(HttpError.badRequest);
 
-    final future = await sut.auth(params: params);
+    final future = sut.auth(params: params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
+  test("Shouldthrow UnexpectedError if HttpClient returns 400", () async {
+    when(
+      httpClient.request(
+        url: "url",
+        method: "anyNamed('method')",
+        body: {
+          'email': faker.internet.email(),
+          'password': faker.internet.password()
+        },
+      ),
+    ).thenThrow(HttpError.notfound);
+
+    final future = sut.auth(params: params);
     expect(future, throwsA(DomainError.unexpected));
   });
 }
